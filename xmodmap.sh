0105e27bfda2200e5fa5ec7db5420231390af64f
step_artur87 <step.artur87@gmail.com>
2017-05-19 15:10:55 +0300

    Desciption

--- xmodmap.sh
+++ xmodmap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#Changes keyboard to use xfce4-accessibility-settings with arrow keys
 xmodmap -e "keycode 111 = Up KP_Up"
 xmodmap -e "keycode 114 = Right KP_Right"
 xmodmap -e "keycode 116 = Down KP_Down"
@@ -8,7 +9,7 @@ xmodmap -e "keycode 94 = F12 F12"
 
 #xmodmap -e "pointer = 2 1 3 4 5 6 7 8 9 10 11 12"
 
-
+xfce4-accessibility-settings
 
 
 
