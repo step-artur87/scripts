37dbb934277c3cb0d9d1583bb8b1b0c8b979cf98
step_artur87 <step.artur87@gmail.com>
2017-05-21 04:20:47 +0300

    Change lsls.sh

--- lsls.sh
+++ lsls.sh
@@ -1,5 +1,6 @@
 IFS=$'\n'       # make newlines the only separator
-for i in $(find $1 | grep -v "\/\.")
+for i in $(find `pwd` -type d | grep -v '/\.')
 	do
-	mkdir -p "/dev/shm/`pwd`/$i"
+	mkdir -p "/dev/shm$i"
+	ls -F1 "$i" > "/dev/shm$i.txt"
 	done
