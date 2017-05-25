b4935f7f13a1582a3ee56502b9c8456b67ddc635
step_artur87 <step.artur87@gmail.com>
2017-05-25 16:41:53 +0300

    Disabled NumLock keys

--- xmodmap.sh
+++ xmodmap.sh
@@ -16,7 +16,18 @@ xmodmap -e "keycode 111 = KP_Up Up"
 xmodmap -e "keycode 114 = KP_Right Right"
 xmodmap -e "keycode 116 = KP_Down Down"
 xmodmap -e "keycode 113 = KP_Left Left"
-
-
-
-
+xmodmap -e "keycode 87 = j"
+xmodmap -e "keycode 88 = k"
+xmodmap -e "keycode 89 = l"
+xmodmap -e "keycode 83 = u"
+xmodmap -e "keycode 84 = i"
+xmodmap -e "keycode 85 = o"
+xmodmap -e "keycode 79 = 7"
+xmodmap -e "keycode 80 = 8"
+xmodmap -e "keycode 81 = 9"
+xmodmap -e "keycode 90 = m"
+xmodmap -e "keycode 91 = period"
+xmodmap -e "keycode 82 = semicolon"
+xmodmap -e "keycode 63 = p"
+xmodmap -e "keycode 106 = 0"
+xmodmap -e "keycode 86 = slash"
