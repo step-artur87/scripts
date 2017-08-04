b2a84bd2d97870ab3543f400d2b63508cd673b7c
step-artur87 <step.artur87@gmail.com>
2017-08-04 21:23:51 +0300

    changed yo-x

--- yo-x.sh
+++ yo-x.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #reverse
-youtube-dl -x --audio-format mp3 --audio-quality 0 -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' $1
+youtube-dl -x -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' $1
