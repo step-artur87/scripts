5d14a2748c5f7276af3041882d02a2a10c186039
step-artur87 <step.artur87@gmail.com>
2017-11-23 15:28:59 +0200

    commit

---
+++ test/streaml.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+/home/art/Downloads/streamlink-twitch-gui/./start.sh &
+echo 0
+sleep 10s
+./w.sh Streamlink 3
---
+++ twitter.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+i=0
+palemoon https://twitter.com/Ale4kko
+read i
+palemoon https://twitter.com/billi_67
+read i
+palemoon https://twitter.com/apiontkovsky
+read i
+palemoon https://twitter.com/Vitauskas_A
+read i
+palemoon https://twitter.com/__Beardman_
+read i
+palemoon https://twitter.com/Judas0803
+read i
+palemoon https://twitter.com/sergonaumovich/media
