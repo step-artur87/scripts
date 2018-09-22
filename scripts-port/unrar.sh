306efab3bcdf00b21227f3367e0d58332cca2897
lazy <lazy>
2018-09-22 14:07:36 +0000

    mouse-reverse-y

--- scripts-port/unrar.sh
+++ scripts-port/unrar.sh
@@ -1,9 +1,9 @@
-cd /mnt/sda3/
+cd /mnt/sda2/archive/
 mkdir extracted
 for i in $(ls  | grep \\.rar ) #how not unzip
 do
 #engrampa --extract-to=/mnt/sda2/guest/dos/ $i
 #mv $i extracted
-engrampa --extract-to=/mnt/sda3/dos/ $i && mv $i extracted
+engrampa --extract-to=/mnt/sda2/dos $i && mv $i extracted
 
 done
