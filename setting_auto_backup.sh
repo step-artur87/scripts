125c4f95c2b6f36c8083c628bffcc28fbb9cb5bf
step-artur87 <step.artur87@gmail.com>
2019-08-26 22:25:33 +0300

    setting_auto_backup

---
+++ setting_auto_backup.sh
@@ -0,0 +1,7 @@
+#not changefirst arg
+cp -r -v \
+-t /mnt/sda3/scripts-smartgit/settings-auto \
+/home/guest/.bashrc \
+/home/guest/.inputrc \
+/home/guest/.livestreamerrc \
+/home/guest/.streamlinkrc
