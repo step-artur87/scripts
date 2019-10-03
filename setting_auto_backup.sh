669ee4fece9c78218a01f0f4f168de4f005db1c4
step-artur87 <step.artur87@gmail.com>
2019-10-03 03:35:02 +0300

    setting_auto_backup.sh----settings-auto-backup/teeworlds1570062787----teew

--- setting_auto_backup.sh
+++ setting_auto_backup.sh
@@ -5,3 +5,5 @@ cp -r -v \
 /home/guest/.inputrc \
 /home/guest/.livestreamerrc \
 /home/guest/.streamlinkrc
+find /mnt/sda2/modules/
+find /mnt/sda2/modules/ -maxdepth 2 >> /mnt/sda3/scripts/settings-auto-backu/modules_list
